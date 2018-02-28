523dbef6f33cdec72e16c6e6a3267d64248b0a10
TrueCodingNinja <thomas.oswald@stud.uni-regensburg.de>
2018-02-28 21:04:32 +0100

    Adding test animated connection

---
+++ Assets/TestV/DrawAnimatedLine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DigitalRuby.AnimatedLineRenderer;
+
+public class DrawAnimatedLine : MonoBehaviour {
+
+    private LineRenderer _line;
+
+	// Use this for initialization
+	void Start () {
+        _line = GetComponent<LineRenderer>();
+        GetComponent<AnimatedLineRenderer>().Enqueue(_line.GetPosition(0));
+        GetComponent<AnimatedLineRenderer>().Enqueue(_line.GetPosition(1));
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
