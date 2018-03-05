0d35ca2e2e8d7572782da864f76a9b2a58663846
TrueCodingNinja <thomas.oswald@stud.uni-regensburg.de>
2018-03-05 23:56:12 +0100

    changing call animation

---
+++ Assets/Scripts/DeviceAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceAnimator : MonoBehaviour {
+
+
+    public Canvas CallIndicator;
+    private Animator _anim;
+
+	// Use this for initialization
+	void Start () {
+        _anim = GetComponent<Animator>();
+	}
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("ANIMATE");
+            Call();
+        }
+    }
+
+    public void Call()
+    {
+        _anim.Play("Call");
+        Transform indicator = Instantiate(CallIndicator, transform).transform;
+        indicator.GetComponent<Canvas>().worldCamera = Camera.main;
+        indicator.LookAt(Camera.main.transform);
+        indicator.localPosition += new Vector3(0f, 0.3f, -1f);
+    }
+}
